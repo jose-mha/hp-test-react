0a838a4cc43a86800e24d53d68e06c8a8174c800
José Manuel Hernández Antaño <jose.hernandez@gonet.us>
2021-12-09 16:36:25 -0600

    :wrench: Add Services
    Se agregaron servicios para las llamadas REST

---
+++ src/services/AppService.js
@@ -0,0 +1,53 @@
+import { API } from './api';
+
+/**
+ ** Clase para el consumo de endpoints implementando API
+ */
+class AppService {
+  url = 'http://localhost:5000/';
+
+  /**
+   * @endpoint /characters?name=
+   */
+  async getCharacterByName(name = '') {
+    return await API.get(`${this.url}characters?name=${name}`);
+  }
+
+  /**
+   * @endpoint /characters
+   */
+  async getCharacters() {
+    return await API.get(`${this.url}characters`);
+  }
+
+  /**
+   * @endpoint /characters
+   */
+  async newCharacter(payload) {
+    return await API.post(`${this.url}characters`, payload);
+  }
+
+  /**
+   * @endpoint /gryffindor
+   */
+  async getGryffindor() {
+    return await API.get(`${this.url}gryffindor`);
+  }
+
+  /**
+   * @endpoint /staff
+   */
+
+  async getStaff() {
+    return await API.get(`${this.url}staff`);
+  }
+
+  /**
+   * @endpoint /students
+   */
+  async getStudents() {
+    return await API.get(`${this.url}students`);
+  }
+}
+
+export default new AppService();
---
+++ src/services/api/index.js
@@ -0,0 +1,61 @@
+import Axios from 'axios';
+/**
+ * * Método base para realizar la llamada al servicio REST.
+ */
+const Request = (method, url, payload) => {
+  payload = payload ? payload : {};
+
+  let { data = {} } = payload;
+
+  //* Creamos el header por default
+  let requestHeaders = {
+    'content-type': 'application/json',
+  };
+
+  //* Realizamos la petición al servicio REST
+  return Axios({
+    method: method,
+    url: url,
+    data: data,
+    headers: requestHeaders,
+  }).then(
+    (response) => {
+      //* Retornamos el resultado
+      return response;
+    },
+    (error) => {
+      throw error;
+    }
+  );
+};
+
+/**
+ ** Definimos el API con los métodos get, post, put, delete y patch
+ */
+export const API = {
+  get: (url, payload) => {
+    payload = payload ? payload : {};
+    let { params = null } = payload;
+    if (params) {
+      return Request('GET', url, payload);
+    } else {
+      return Request('GET', url, { params });
+    }
+  },
+
+  post: (url, payload) => {
+    return Request('POST', url, payload);
+  },
+
+  put: (url, payload) => {
+    return Request('PUT', url, payload);
+  },
+
+  patch: (url, payload) => {
+    return Request('PATCH', url, payload);
+  },
+
+  delete: (url, payload) => {
+    return Request('DELETE', url, payload);
+  },
+};
